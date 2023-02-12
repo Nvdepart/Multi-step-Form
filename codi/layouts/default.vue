78247e8ec9eeed3ce13d58f7c0996b066822ce8d
Nvdepart <67013480+Nvdepart@users.noreply.github.com>
2023-02-12 23:18:06 +0100

    installation kitform

---
+++ codi/layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app dark>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+
+    <v-footer>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {};
+</script>
